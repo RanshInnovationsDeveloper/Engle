fa9ce68e5fac0a447e360050e2a18d35eb65ded4
Sumitkumar104 <parjapatsumit104@gmail.com>
2024-04-26 23:03:37 +0530

    some minor change in solve issue of .env file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
-import { Routes, Route, useLocation } from "react-router-dom";
-import { useEffect } from "react";
+import { Routes, Route} from "react-router-dom";
 import Homepage from './pages/Homepage'
 import './App.css'
 import Loginpage from './pages/Loginpage'
@@ -11,152 +10,120 @@ import AboutUspage from './pages/AboutUspage'
 import FlashCardpage from './pages/FlashCardpage'
 import FavouritesPage from './pages/FavouritesPage'
 import MynotesPage from "./pages/MynotesPage";
-import Notecard from "./components/Notecard";
-import Stories from "./pages/Stories";
-import IndividualStory from "./pages/IndividualStory";
 import PrivateRoute from "./components/auth/PrivateRoute";
 import OpenRoute from "./components/auth/OpenRoute";
 import Upcomingpage from "./pages/Upcomingpage";
 import Spinner from "./components/Spinner";
 import FavouritesCategoryPage from "./pages/FavouritesCategoryPage.jsx"
-import { setLoading } from './slices/authSlice';
-import { useDispatch, useSelector } from "react-redux"
+import { useSelector } from "react-redux"
 import ErrorPage from "./pages/ErrorPage.jsx";
 import SomethingwentwrongPage from "./pages/SomethingwentwrongPage.jsx";
 import EmailVerificationPage from "./pages/EmailVerificationPage.jsx";
 
 function App() {
-    const { loading } = useSelector((state) => state.auth);
-    const dispatch = useDispatch();
-    const location = useLocation();
-
-
-    // useEffect(() => {
-    //   dispatch(setLoading(true));
-
-    //     const loadingTimer = setTimeout(() => {
-    //       dispatch(setLoading(false));
-    //     }, 100); 
-
-    //     return () => clearTimeout(loadingTimer); 
-    // }, [location.pathname,dispatch]);
-
+  const { loading } = useSelector(state => state.auth);
 
   return (
     <>
-    {loading ? (
-      <Spinner/>
-    ): (
-      <>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/contact" element={<ContactUspage />} />
-        <Route path="/about" element={<AboutUspage />} />
-        <Route path="/flashcards" element={<FlashCardpage />} />
-        <Route path="/ambiguous_words" element={< Upcomingpage />} />
-        <Route path="/learn_with_context" element={< Upcomingpage />} />
-        <Route path="/learn_with_story" element={< Upcomingpage />} />
-        <Route path="/learn_with_friends" element={< Upcomingpage />} />
-        <Route path="/error" element={< SomethingwentwrongPage />} />
-        <Route path="*" element={< ErrorPage />} />
+      {loading ? (
+        <Spinner />
+      ) : (
+        <>
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/contact" element={<ContactUspage />} />
+            <Route path="/about" element={<AboutUspage />} />
+            <Route path="/flashcards" element={<FlashCardpage />} />
+            <Route path="/ambiguous_words" element={< Upcomingpage />} />
+            <Route path="/learn_with_context" element={< Upcomingpage />} />
+            <Route path="/learn_with_story" element={< Upcomingpage />} />
+            <Route path="/learn_with_friends" element={< Upcomingpage />} />
+            <Route path="/error" element={< SomethingwentwrongPage />} />
+            <Route path="*" element={< ErrorPage />} />
 
-        {/* <Route path="/story" element={<Stories/>} /> */}
-        {/* <Route path="/story/:id" element={<IndividualStory/>} /> */}
+            <Route
+              path="/login"
+              element={
 
+                <OpenRoute>
+                  <Loginpage />
+                </OpenRoute>
 
-        <Route
-          path="/login"
-          element={
+              }
+            />
 
-            <OpenRoute>
-              <Loginpage />
-            </OpenRoute>
+            <Route
+              path="/register"
+              element={
 
-          }
-        />
+                <OpenRoute>
+                  <Registerpage />
+                </OpenRoute>
 
-        <Route
-          path="/register"
-          element={
+              }
+            />
 
-            <OpenRoute>
-              <Registerpage />
-             </OpenRoute>
+            <Route
+              path="/forgotpass"
+              element={
 
-          }
-        />
+                <OpenRoute>
+                  <ForgotPasspage />
+                </OpenRoute>
 
-        <Route
-          path="/forgotpass"
-          element={
-           
-             <OpenRoute>
-              <ForgotPasspage/>
-             </OpenRoute> 
+              }
+            />
+            <Route
+              path="/verifyemail"
+              element={
 
-          }
-        />
-   <Route
-          path="/verifyemail"
-          element={
-           
-             <OpenRoute>
-              <EmailVerificationPage/>
-             </OpenRoute> 
+                <OpenRoute>
+                  <EmailVerificationPage />
+                </OpenRoute>
 
-          }
-        />
-        <Route
-          path="/proflie"
-          element={
+              }
+            />
 
-            <PrivateRoute>
-              {/* < profile /> */}
-            </PrivateRoute>
+            <Route
+              path="/favourites/:type"
+              element={
 
-          }
-        />
+                <PrivateRoute>
+                  <FavouritesPage />
+                </PrivateRoute>
 
-        <Route
-          path="/favourites/:type"
-          element={
+              }
+            />
+            <Route
+              path="/favourites/"
+              element={
 
-            <PrivateRoute>
-              <FavouritesPage />
-            </PrivateRoute>
+                <PrivateRoute path={"/favourites/"}>
+                  <FavouritesCategoryPage />
+                </PrivateRoute>
 
-          }
-        />
-        <Route
-          path="/favourites/"
-          element={
-
-            <PrivateRoute path={"/favourites/"}>
-              <FavouritesCategoryPage />
-            </PrivateRoute>
-
-          }
-        />
+              }
+            />
 
 
-        <Route
-          path="/mynotes"
-          element={
+            <Route
+              path="/mynotes"
+              element={
 
-            <PrivateRoute path={"/mynotes"}>
-              <MynotesPage />
-            </PrivateRoute>
+                <PrivateRoute path={"/mynotes"}>
+                  <MynotesPage />
+                </PrivateRoute>
 
-          }
-        />
-      
-      </Routes>
-      <ToastContainer position="bottom-center" reverseOrder={false} autoClose={2000} hideProgressBar={true} />
+              }
+            />
+
+          </Routes>
+          <ToastContainer position="bottom-center" reverseOrder={false} autoClose={2000} hideProgressBar={true} />
+        </>
+      )}
     </>
-    )}
-    </>
-    
-    
+
+
   );
 };
 
