3368511eb15b2144e5f091e7451ccc475f99fe5f
RanshInnovationsDeveloper <168216748+RanshInnovationsDeveloper@users.noreply.github.com>
2024-05-03 00:46:05 +0530

    Delete server directory

--- server/__mocks__/axios.js
+++
@@ -1,6 +0,0 @@
-// __mocks__/axios.js
-
-module.exports = {
-    get: jest.fn(),
-  };
-  
--- server/routes/wordRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const {fetchWord}=require("../controllers/wordGeneratorControllers");
-
-
-router.post("/fetchword",fetchWord);
-
-module.exports = router;
