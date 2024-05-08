3dcd8ff175c0b8bb66fa0185c54c75bfbd225989
Sanchita <sakshibhardwaj14@gmail.com>
2024-05-08 11:47:59 +0530

    Changes in favicon, profile dropdown and signup.

--- frontend/src/components/ProfileDropdown.jsx
+++ frontend/src/components/ProfileDropdown.jsx
@@ -23,7 +23,8 @@ const ProfileDropdown = ({ isOpen, isMobile}) => {
     dispatch(setLoading(false));
 
   }
-
+  
+    const truncatedUserName = userName.slice(0, 10) + '...';  
 
   return (
     <div>
@@ -32,10 +33,8 @@ const ProfileDropdown = ({ isOpen, isMobile}) => {
         <div  className="absolute mt-3 z-20  ">
 
           <div className="bg-white border w-[10rem] border-[#001F92]  flex flex-col ">
-          <div className={` px-4 py-2 text-center bg-[#EBEDFF]  h-[3rem] border-b border-[#5B7ADE] `}>
-                {userName}
-              
-
+            <div className={` px-4 py-2 text-center bg-[#EBEDFF]  h-[3rem] border-b border-[#5B7ADE] `}>
+                {(userName.length > 10)? truncatedUserName : userName}
               </div>
               <div onClick={handlesignout} className={` px-4 py-2 text-center cursor-pointer  hover:shadow-md  bg-[#EBEDFF]  hover:bg-[#FFFFFF] h-[3rem] `}>
                 <button
